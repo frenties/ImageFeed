40801074a253560e35cfe134c70415ab13056d07
frenties <frenties@yandex.ru>
2026-08-25 15:47:44 +0300

    feat: Implement SplashViewController and login flow navigation

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+protocol AuthViewControllerDelegate: AnyObject {
+    func didAuthenticate(_ vc:AuthViewController)
+}
+
+final class AuthViewController: UIViewController {
+    weak var delegate: AuthViewControllerDelegate?
+    
+    private let oauth2Service = OAuth2Service.shared
+    
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+    
+    func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named:"nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named:"ypBlack")
+    }
+    
+    private func fetchOAuthToken(with code: String) {
+        oauth2Service.fetchOAuthToken(with: code) { result in
+        }
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        vc.dismiss(animated: true)
+        
+        oauth2Service.fetchOAuthToken(with: code) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let token):
+                self.delegate?.didAuthenticate(self)
+            case .failure(let error):
+                print("Ошибка авторизации: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
+}
+
---
+++ ImageFeed/OAuth2/OAuth2TokenStorage.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+final class OAuth2TokenStorage {
+    private let tokenKey = "OAuth2AccessToken"
+    
+    var token: String? {
+        get {
+            return UserDefaults.standard.string(forKey: tokenKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: tokenKey)
+        }
+    }
+}
